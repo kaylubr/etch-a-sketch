df15b7a54513f476b27b31704dd30c1de7599ff1
kaylubr <kylebreyes092005@gmail.com>
2024-07-25 15:32:50 +0800

    Add a deleteCanvas function
    This function deletes pixels to reset the canvas

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ let pixels;
 let color = "white";
 
 function setPixels(size) {
-
-    for (let rows = 1; rows < size; rows++) {
-        for (let column = 0; column < size.length; column++) {
+    deleteCanvas();
+    for (let rowCount = 1; rowCount < size; rowCount++) {
+        for (let columnCount = 0; columnCount < size.length; columnCount++) {
             pixels = document.createElement("div");
             pixels.classList.add("pixels");
             row.appendChild(pixels);
         }
         
+        row = row.cloneNode(true);
+        canvas.appendChild(row);
+    }
+}
 
+function deleteCanvas() {
+    while(canvas.hasChildNodes) {
+        while(row.hasChildNodes) {
+            row.removeChild(row.firstElementChild);
+        }
+        canvas.removeChild(canvas.firstElementChild);
     }
 }
