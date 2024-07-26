d1cf15f8314fb772ad82f5413becd566ee757a2a
kaylubr <kylebreyes092005@gmail.com>
2024-07-26 12:19:13 +0800

    Made a seperate function for the tools except line tool
    Newest modification is rainbow tool and darken tool

--- script.js
+++ script.js
@@ -23,21 +23,38 @@ toolsKit.addEventListener("click", (event) => {
     switch(toolUsed) {
         case "pen":
             tool = "pen";
+            canvas.addEventListener("mouseover", penFunction);
+            removeEventsExcept("pen");
+            console.log(tool);
             break;
         case "eraser":
             tool = "eraser";
+            canvas.addEventListener("mouseover", eraserFunction);
+            removeEventsExcept("eraser");
+            console.log(tool);
             break;  
         case "bucket":
             tool = "bucket";
+            canvas.addEventListener("click", bucketFunction);
+            removeEventsExcept("bucket");
+            console.log(tool);
             break;
         case "darken":
             tool = "darken";
+            setOpacityToZero();
+            canvas.addEventListener("mouseover", darkenFunction);
+            removeEventsExcept("darken");
+            console.log(tool);
             break;
         case "rainbow":
             tool = "rainbow";
+            canvas.addEventListener("mouseover", rainbowFunction);
+            removeEventsExcept("rainbow");
+            console.log(tool);
             break;  
         case "line":
             tool = "line";
+            console.log(tool);
             break;        
     }
 });
@@ -55,6 +72,8 @@ colorTools.addEventListener("click", (event) => {
         color = "rgb(201, 134, 145)";
     }
 
+    if (tool === "darken") setOpacityToZero();
+
     console.log(color);
 });
 
@@ -65,13 +84,14 @@ trashBtn.addEventListener("click", (event) => {
 
     pixel.forEach(cell => {
         cell.style.backgroundColor = "white";
+        cell.style.opacity = "100%";
     });
 
 })
 
 //grid for canvas
 function makeGrid(size) {
-    let uniqueId = 1;
+    let uniqueCount = 1;
     for (let rowCount = 0; rowCount < size; rowCount++) {
         let row = document.createElement("div");
         canvas.appendChild(row).className = "row";
@@ -79,8 +99,8 @@ function makeGrid(size) {
         for (let cells = 0; cells < size; cells++) {
             let cell = document.createElement("div");
             row.appendChild(cell).className = "pixel";
-            cell.setAttribute("id", uniqueId);
-            uniqueId++;
+            cell.setAttribute("id", uniqueCount.toString());
+            uniqueCount++;
         }
     }
 }
@@ -117,4 +137,103 @@ function promptGridSize() {
 
 function getPixels() {
     return document.querySelectorAll(".pixel");
+}
+
+const penFunction = (e) => {
+    let target = e.target.id;
+    let uniqueId = document.getElementById(target);
+    uniqueId.style.backgroundColor = color;
+    uniqueId.style.opacity = "100";
+}
+
+const eraserFunction = (e) => {
+    let target = e.target.id;
+    let uniqueId = document.getElementById(target);
+    uniqueId.style.backgroundColor = "white";
+    uniqueId.style.opacity = "100%";
+}
+
+const bucketFunction = (e) => {
+    let whole = getPixels();
+    whole.forEach(pix => {
+        pix.style.backgroundColor = color;
+        pix.style.opacity = "100%";
+    });
+}
+
+const rainbowFunction = (e) => {
+    let target = e.target.id;
+    let uniqueId = document.getElementById(target);
+    uniqueId.style.backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+}
+
+let opacityPercent = 0;
+const darkenFunction = (e) => {
+    let target = e.target.id;
+    let uniqueId = document.getElementById(target);
+    uniqueId.style.background = color;
+
+    if (opacityPercent >= 100) {
+        opacityPercent = 100;
+    }
+    uniqueId.style.opacity = `${opacityPercent}%`;
+    opacityPercent = opacityPercent + 10;
+    console.log(uniqueId);
+}
+
+function removeEventsExcept(tool) {
+    if (tool === "pen") {
+        canvas.removeEventListener("mouseover", eraserFunction);
+        canvas.removeEventListener("click", bucketFunction);
+        canvas.removeEventListener("mouseover", rainbowFunction);
+        canvas.removeEventListener("mouseover", darkenFunction);
+        // canvas.removeEventListener("mouseover", lineFunction);
+    }
+    else if (tool === "eraser") {
+        canvas.removeEventListener("mouseover", penFunction);
+        canvas.removeEventListener("click", bucketFunction);
+        canvas.removeEventListener("mouseover", rainbowFunction);
+        canvas.removeEventListener("mouseover", darkenFunction);
+        // canvas.removeEventListener("mouseover", lineFunction);
+    }
+    else if (tool === "bucket") {
+        canvas.removeEventListener("mouseover", eraserFunction);
+        canvas.removeEventListener("mouseover", penFunction);
+        canvas.removeEventListener("mouseover", rainbowFunction);
+        canvas.removeEventListener("mouseover", darkenFunction);
+        // canvas.removeEventListener("mouseover", lineFunction);
+    }
+    else if (tool === "rainbow") {
+        canvas.removeEventListener("mouseover", eraserFunction);
+        canvas.removeEventListener("click", bucketFunction);
+        canvas.removeEventListener("mouseover", penFunction);
+        canvas.removeEventListener("mouseover", darkenFunction);
+        // canvas.removeEventListener("mouseover", lineFunction);
+    }
+    else if (tool === "darken") {
+        canvas.removeEventListener("mouseover", eraserFunction);
+        canvas.removeEventListener("click", bucketFunction);
+        canvas.removeEventListener("mouseover", rainbowFunction);
+        canvas.removeEventListener("mouseover", penFunction);
+        // canvas.removeEventListener("mouseover", lineFunction);
+    }
+    else if (tool === "line") {
+        canvas.removeEventListener("mouseover", eraserFunction);
+        canvas.removeEventListener("click", bucketFunction);
+        canvas.removeEventListener("mouseover", rainbowFunction);
+        canvas.removeEventListener("mouseover", darkenFunction);
+        canvas.removeEventListener("mouseover", penFunction);
+    }
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 255);
+}
+
+function setOpacityToZero() {
+    opacityPercent = 0;
+}
+
+function setOpacityToOne() {
+    opacityPercent = 100;
 }
